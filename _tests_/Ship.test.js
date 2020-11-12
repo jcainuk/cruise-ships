58d702d027e3191f5accaa82e54085b5a75a64a5
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-11-12 14:16:58 +0000

    Saved Ship.test.js

--- _tests_/Ship.test.js
+++ _tests_/Ship.test.js
@@ -29,15 +29,3 @@ it('can set sail', () => {
 });
 });
 
-
-describe('Port', () => {
-	it('can be instantiated', () => {
-		expect(new Port()).toBeInstanceOf(Object);
-	});
-
-	it('has a name property', () => {
-		const port = new Port('Calais');
-		expect(port).toHaveProperty('name','Calais');
-	});
-
-});
