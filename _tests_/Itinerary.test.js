b00a64dc431935d852781a53705fea6d93a28584
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-11-12 15:16:41 +0000

    Wrote test in Itinerary.test.js

--- _tests_/Itinerary.test.js
+++ _tests_/Itinerary.test.js
@@ -2,3 +2,14 @@ const Ship = require('../src/Ship.js');
 const Port = require('../src/Port.js');
 const Itinerary = require('../src/Itinerary.js');
 
+describe('Itinerary', () => {
+	it('can have ports', () => {
+        const dover = new Port('Dover');
+        const calais = new Port('Calais');
+
+        const itinerary = new Itinerary(dover, calais);
+
+		expect(itinerary.ports.toEqual([dover, calais]);
+	});
+
+});
