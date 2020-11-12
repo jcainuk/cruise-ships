30fcffa6203853ff7577738dd0b20f3071891c61
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-11-12 15:36:43 +0000

    Corrected itinerary test file

--- _tests_/Itinerary.test.js
+++ _tests_/Itinerary.test.js
@@ -3,6 +3,21 @@ const Port = require('../src/Port.js');
 const Itinerary = require('../src/Itinerary.js');
 
 describe('Itinerary', () => {
+
+    it('can be instantiated', () => {
+		expect(new Itinerary()).toBeInstanceOf(Object);
+    });
+    
+    it('has a ports property', () => {
+       
+        const dover = new Port('Dover');
+        const calais = new Port('Calais');
+
+        const itinerary = new Itinerary([calais, dover]);
+
+		expect(itinerary).toHaveProperty('ports');
+	});
+
 	it('can have ports', () => {
         const dover = new Port('Dover');
         const calais = new Port('Calais');
