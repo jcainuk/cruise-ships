2d9e25e2564df9d32eea91142ab3472beab73bab
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-11-16 14:16:51 +0000

    Added this.currentPort.addShip(this) to Ship constructor function

--- src/Ship.js
+++ src/Ship.js
@@ -3,6 +3,7 @@ function Ship (itinerary) {
   this.itinerary = itinerary;
   this.currentPort = itinerary.ports[0];
   this.previousPort = null;
+  this.currentPort.addShip(this);
 }
 
 Ship.prototype.setSail = function () {
