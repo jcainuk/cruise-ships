5c23ecdafb3ab5e10880800adccbd20903f5bf16
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-11-16 13:57:37 +0000

    Wrote function for addShip on Port.js

--- src/Port.js
+++ src/Port.js
@@ -1,7 +1,13 @@
 function Port(name){
 
     this.name = name;
+    this.ships = [];
 }
+
+Port.prototype.addShip = function (ship) {
+   this.ships.push(ship);
+     
+};
 
 
 module.exports = Port;
