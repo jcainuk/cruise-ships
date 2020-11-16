56631c87e52ec1f159fd96d6fbb9758778cf92b3
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-11-16 12:56:27 +0000

    Changed ship setSail method

--- src/Ship.js
+++ src/Ship.js
@@ -7,6 +7,13 @@ function Ship (itinerary) {
 
 Ship.prototype.setSail = function () {
    
+    const itinerary = this.itinerary;
+    const currentPortIndex = itinerary.ports.indexOf(this.currentPort);
+  
+    if (currentPortIndex === (itinerary.ports.length - 1)) {
+      throw new Error('End of itinerary reached');
+    }
+  
     this.previousPort = this.currentPort;
     this.currentPort = null;
     
