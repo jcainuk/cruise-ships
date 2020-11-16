d7bcff1ebd356cefb2e8c7a87414757ec9a59475
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-11-16 15:51:43 +0000

    Refactored Port test

--- _tests_/Port.test.js
+++ _tests_/Port.test.js
@@ -4,12 +4,19 @@ const Port = require('../src/Port.js');
 const Itinerary = require('../src/Itinerary.js');
 
 describe('Port', () => {
+	//
+	let port;
+
+    beforeEach(() => {
+        port = new Port('Calais');
+    });
+	//
 	it('can be instantiated', () => {
 		expect(new Port()).toBeInstanceOf(Object);
 	});
 
 	it('has a name property', () => {
-		const port = new Port('Calais');
+		//const port = new Port('Calais');
 		expect(port).toHaveProperty('name','Calais');
 	});
 
